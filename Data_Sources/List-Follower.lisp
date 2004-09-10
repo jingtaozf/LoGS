8e0a92aad361de06a43a6c9ba6318d18e51c22e1
LoGS <LoGS>
2004-09-10 22:22:49 +0000

    Data-Source is the new base class for all data sources (including File-Follower).
    List-Follower is there for testing purposes.

---
+++ Data_Sources/List-Follower.lisp
@@ -0,0 +1,33 @@
+; Logs extensible (common-lisp based) log/event analysis engine/language
+; Copyright (C) 2003-2004 James Earl Prewett
+
+; This program is free software; you can redistribute it and/or
+; modify it under the terms of the GNU General Public License
+; as published by the Free Software Foundation; either version 2
+; of the License, or (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(defclass List-Follower (Data-Source)
+  ((message-list :accessor message-list 
+                 :initarg :message-list
+                 :initform ())))
+
+
+(defmethod get-line ((List-Follower List-Follower))
+  (let ((first-message (car (message-list List-Follower))))
+    (setf (message-list List-Follower)
+          (cdr (message-list List-Follower)))
+    first-message))
+
+(defmethod get-logline ((List-Follower List-Follower))
+  (let ((line (get-line List-Follower)))
+    (when line
+      (make-instance 'message :message line))))
