d02cbaf73880f009a6662debf2a1076fbcc06e6c
LoGS <LoGS>
2005-01-25 14:53:40 +0000

    updated build scripts for Edi's new version of cl-ppcre: 1.2.1

--- Build_LoGS.sh
+++ Build_LoGS.sh
@@ -18,7 +18,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 
-CL_PPCRE="cl-ppcre-1.1.0/load.lisp"
+CL_PPCRE="cl-ppcre-1.2.1/load.lisp"
 LOGS="LoGS.lisp"
 LOGS_CORE="LoGS.core"
 
