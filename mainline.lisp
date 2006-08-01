6fb4de7e85d1f16bd39980aac2ad1277c271340e
LoGS <LoGS>
2006-08-01 21:35:18 +0000

    added copyright notice to mainline
    added back (quit-logs) to end of MAIN

--- mainline.lisp
+++ mainline.lisp
@@ -1,7 +1,19 @@
+;;;; Logs extensible (common-lisp based) log/event analysis engine/language
+;;;; Copyright (C) 2003-2006 James Earl Prewett
 
-;; insert GPL notice here
-
-;; Copyright 2006 James E. Prewett
+;;;; This program is free software; you can redistribute it and/or
+;;;; modify it under the terms of the GNU General Public License
+;;;; as published by the Free Software Foundation; either version 2
+;;;; of the License, or (at your option) any later version.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program; if not, write to the Free Software
+;;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 (in-package :org.prewett.LoGS)
 
@@ -35,8 +47,7 @@
        (funcall function))
      *run-before-exit*)
     ;; exit LoGS
-    ; (quit-LoGS)
-    ))
+    (quit-LoGS)))
 
 
 
@@ -61,6 +72,7 @@
 
      if *message*
      do
+       (LoGS-debug "got message: ~A~%" (IF *MESSAGE* (message *message*)))
        (check-rules *message* *root-ruleset*)
 
      else
