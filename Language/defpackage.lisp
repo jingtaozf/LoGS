9167bae141825898202f8ebe23a224b4cb2dbb79
lvijay <lvijay>
2006-12-20 05:29:13 +0000

    Since the RDL uses more than one file, I think it's best we keep its package definition in a separate file.

---
+++ Language/defpackage.lisp
@@ -0,0 +1,28 @@
+;;;; LoGS extensible (common-lisp based) log/event analysis engine/language
+;;;; Copyright (C) 2006 Vijay Lakshminarayanan
+
+;;;; This file is a part of LoGS.  LoGS is free software; you can
+;;;; redistribute it and/or modify it under the terms of the GNU
+;;;; General Public License as published by the Free Software
+;;;; Foundation; either version 2 of the License, or (at your option)
+;;;; any later version.
+
+;;;; This file is a part of LoGS.  The copyright will soon be
+;;;; transferred to the author of LoGS, James Earl Prewett.
+
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program; if not, write to the Free Software
+;;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(defpackage :org.prewett.LoGS.language
+  (:use #:cl #:cybertiggyr)
+  (:import-from #:logs #:rule #:ruleset #:message #:exec #:context)
+  (:import-from #:cybertiggyr-time #:make-broken-time)
+  (:nicknames #:language))
+
+;;; *eof*
