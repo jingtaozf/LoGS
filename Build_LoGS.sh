a01fd73ab9b33e3f873e75b29c5879eec6a5499b
LoGS <LoGS>
2008-05-27 02:01:28 +0000

    updated copyright information

--- Build_LoGS.sh
+++ Build_LoGS.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Logs extensible (common-lisp based) log/event analysis engine/language
-# Copyright (C) 2003-2007 James Earl Prewett
+# Copyright (C) 2003-2008 James Earl Prewett
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
--- cl-cli/cl-cli.lisp
+++ cl-cli/cl-cli.lisp
@@ -1,5 +1,5 @@
 ; CL-CLI Command line processing utility
-; Copyright (C) 2003-2007 James Earl Prewett
+; Copyright (C) 2003-2008 James Earl Prewett
 
 ; This program is free software; you can redistribute it and/or
 ; modify it under the terms of the GNU General Public License
